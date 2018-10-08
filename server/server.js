05a822fb6dff758703c03c51781082ae6de6737f
Alvaro <dax7xad@gmail.com>
2018-10-07 18:13:02 -0600

    segundo commit

--- server/server.js
+++ server/server.js
@@ -43,6 +43,6 @@ app.delete('/usuario', function (req, res) {
     res.json('delete Usuario');
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando el puerto:', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto:', process.env.PORT);
 });
