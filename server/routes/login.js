589d8bf82af142d3008986a3ffe1a0b536fdfb45
Alvaro <dax7xad@gmail.com>
2018-10-29 20:57:15 -0600

    terminando la proteccion de usuario con token

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,14 +2,58 @@ const express = require("express");
 
 const bcrypt = require("bcryptjs");
 
+const jwt = require('jsonwebtoken');
+
 const Usuario = require("../models/usuario");
 
 const app = express();
 
 app.post("/login", (req, res) => {
-  res.json({
-    ok: true
-  });
+
+    let body = req.body;
+
+    Usuario.findOne({
+        email: body.email
+    }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: '(Usuario) o contraseña incorrectos'
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
+        }
+
+        const token = jwt.sign({
+            usuario: usuarioDB
+        }, process.env.SEED, {
+            expiresIn: process.env.CADUDAD_TOKEN
+        });
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        });
+    });
+
 });
 
 module.exports = app;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,9 +3,11 @@ const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificaToken  } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken,  (req, res) => {
 
     let desde = req.query.desde || 0;
     let limite = req.query.limite || 5;
